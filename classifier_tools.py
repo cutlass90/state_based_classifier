49c2923d4076b7adeb5e046c6e34a5c97a5f942a
nazars <cutlass90@mail.ru>
2017-03-02 15:33:14 +0200

    add generating confusion matrix

--- classifier_tools.py
+++ classifier_tools.py
@@ -4,15 +4,17 @@ import os, fnmatch
 import time
 from random import shuffle
 import functools
+import itertools
 
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 import matplotlib
-#matplotlib.use("Agg")
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import tqdm as tqdm
 from sklearn.metrics import confusion_matrix
+
 
 import misc
 from parameters import parameters as PARAM
@@ -382,6 +384,45 @@ def gathering_data_from_chunks(data, list_of_res, overlap=700, n_chunks=32):
     assert data['events'].shape[0] == predicted_events.shape[0], 'Original shape not equal reconstarct shape {0} != {1}'.format(data['events'].shape[0], predicted_events.shape[0])
     return predicted_events
 
+
+def plot_confusion_matrix(true_labels, pred_labels, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues,
+                          save_path = None):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    cm = confusion_matrix(true_labels, pred_labels)
+    np.set_printoptions(precision=2)
+    plt.figure(figsize=(17, 17))
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=90)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+    # Plot normalized confusion matrix
+    if save_path is not None:
+        plt.savefig(save_path)
+    else:
+        plt.show()
 #######################################################################################
 #testing
 """
--- desktop.py
+++ desktop.py
@@ -2,6 +2,9 @@ import os
 
 import tensorflow as tf
 import numpy as np
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
 
 from parameters import parameters as PARAM
 import classifier_tools as utils
@@ -20,23 +23,24 @@ os.makedirs('summary/', exist_ok = True)
 #path_to_train_data = '../../data/little/'
 #path_to_train_data      = '../../data/train/chunked/'
 path_to_train_data      = '../../../ECG_DATA/all/chunked_data/'
+path_to_train_data = '../../data/small_set/'
 path_eval_cost_data     = '../../../ECG_DATA/ECG_DATA_1000samples_2/test/'
 path_to_predict_data    = path_to_train_data
 path_to_predictions     = 'predictions/'
 os.makedirs(path_to_predictions, exist_ok = True)
-n_iter_train            = 50000
+n_iter_train            = 9000
 n_iter_eval             = 10000
-save_model_every_n_iter = 9000
+save_model_every_n_iter = 10000
 path_to_model = 'models/cl'
 
-"""
+
 gen_params = dict(n_frames = PARAM['n_frames'],
                 overlap = PARAM['overlap'],
                 get_data = not(PARAM['use_delta_coding']),
                 get_delta_coded_data = PARAM['use_delta_coding'],
                 get_events = True,
                 rr = PARAM['rr'])
-
+"""
 # Initialize data loader for training
 data_loader = utils.LoadDataFileShuffling(batch_size=PARAM['batch_size'],
                                     path_to_data=path_to_train_data,
@@ -62,12 +66,13 @@ with Classifier(batch_size = PARAM['batch_size'],
 """
 
 # Predicting
-PARAM['use_chunked_data'] = False
+#PARAM['use_chunked_data'] = False
 
-path_to_filenames = '/media/host1/Data/Work/ECG_DATA/all/valid_files/100.txt'
-with open(path_to_filenames, 'r') as file:
-    paths = file.read().splitlines()[:100]
-#paths = utils.find_files(path_to_predict_data, '*.npy')
+#path_to_filenames = '/media/host1/Data/Work/ECG_DATA/all/valid_files/100.txt'
+#with open(path_to_filenames, 'r') as file:
+    #paths = file.read().splitlines()[:100]
+paths = utils.find_files(path_to_predict_data, '*.npy')
+
 
 with Classifier(batch_size = 1,
                 n_beats = PARAM['n_frames'],
@@ -86,10 +91,27 @@ with Classifier(batch_size = 1,
 
 
 # create logs
+true_l, pred_l = [], []
 for path in paths:
     data = np.load(path).item()
     true_events = data['events'][:, np.in1d(data['disease_name'], PARAM['required_diseases'])]
     pred_events = np.load(path_to_predictions + os.path.basename(path)[:-4]+"_events.npy")
+    pred_events_l = (pred_events > 0.5).astype(int)
+    true_ev = np.hstack((true_events, (true_events.sum(1)[:,None]==0).astype(int)))
+    pred_ev = np.hstack((pred_events_l, (pred_events_l.sum(1)[:,None]==0).astype(int)))
+    true_labels = [np.nonzero(true_ev[r])[0][0] for r in range(true_events.shape[0])]\
+        + list(range(len(PARAM['disease_names'])))
+    pred_labels = [np.nonzero(pred_ev[r])[0][0] for r in range(pred_events.shape[0])]\
+        + list(range(len(PARAM['disease_names'])))
+    pred_l += pred_labels
+    true_l += true_labels
+    utils.plot_confusion_matrix(true_labels, pred_labels,
+        classes=np.insert(PARAM['disease_names'], len(PARAM['disease_names']), 'not_event'),
+        normalize=False,
+        title='Confusion matrix',
+        cmap=plt.cm.Blues,
+        save_path = path_to_predictions + os.path.basename(path)[:-4]+"_CM.jpg")
+        #save_path = path_to_predictions + os.path.basename(path)[:-4]+"_CM.jpg")
     utils.save_log(
         path = path_to_predictions,
         file_name = os.path.basename(path)[:-4] + '.csv',
@@ -99,9 +121,17 @@ for path in paths:
         cost = np.arange(len(PARAM['required_diseases'])),
         threshold = 0.5)
 
+
 # create summaty
 data = np.load(paths[0]).item()
 diseases=data['disease_name'][np.in1d(data['disease_name'], PARAM['required_diseases'])]
 utils.save_summary(path=path_to_predictions, diseases=diseases)
+
+utils.plot_confusion_matrix(true_l, pred_l,
+    classes=np.insert(PARAM['disease_names'], len(PARAM['disease_names']), 'not_event'),
+    normalize=False,
+    title='Confusion matrix',
+    cmap=plt.cm.Blues,
+    save_path = "summary_CM.jpg")
 
 
